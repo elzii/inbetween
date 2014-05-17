04f9fa3666c2abf5f1c39c3d21faf5b306f27d3f
Alexander Zizzo <elzi@nerdi.net>
2014-05-16 20:25:10 -0700

    about

--- assets/js/app.js
+++ assets/js/app.js
@@ -50,6 +50,7 @@
     app.setElements();
     app.scroll();
     app.chromeFix('offset');
+    app.lazyLoadVideo();
   }
 
   app.setElements = function()
@@ -88,6 +89,49 @@
           chrome_fix_2();
       }
     }
+  }
+
+
+  app.lazyLoadVideo = function() {
+
+      $('.lazyload').click(function(){
+          var $this           = $(this),
+              video_id        = $this.data('video-id'),
+              video_service   = $this.data('video-service'),
+              width           = $this.width(),
+              height          = $this.height();
+
+              // console.log(width, height);
+
+          // embed code builder function
+          function buildEmbed(service, id) {
+              if (service === 'youtube') {
+                  var embed = '<iframe width="'+width+'" height="'+height+'" src="//www.youtube.com/embed/'+video_id+'?autoplay=1" frameborder="0" allowfullscreen></iframe>';
+                  return embed;
+              }
+              if (service === 'vimeo') {
+                  var embed = '<iframe src="//player.vimeo.com/video/'+video_id+'?title=0&amp;autoplay=1&amp;byline=0&amp;portrait=0&amp;badge=0" width="'+width+'" height="'+height+'" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+                  return embed;
+              }
+          }
+
+          // build the embed code
+          var embed_code = buildEmbed(video_service, video_id);
+
+          // remove inner elements
+          $this.find('iframe').remove();
+          $this.find('img').remove();
+          // $this.find('.media-image-overlay').css('opacity', 0);
+          $this.find('.video-play-button').hide();
+          $this.css('background-image', '');
+
+          // append the generated embed code
+          if ( $this.find('iframe').length <= 0 ) {
+              $this.append(embed_code);
+          } else {
+              // nothing
+          }
+      });
   }
 
 
